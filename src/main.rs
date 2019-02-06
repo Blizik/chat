beb1d2b720abed4b3689d729a21b6131269a662a
Blizik <portalblizik@gmail.com>
2019-02-05 22:08:54 -0500

    Restarted to make more progress

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,94 @@
 #![allow(dead_code, unused_imports)]
 
 extern crate mio;
+extern crate slab;
 
-use std::io::prelude::*;
+use std::io::{self, prelude::*};
 
 use mio::*;
 use mio::net::{TcpListener, TcpStream};
 
+use slab::Slab;
+
+const LISTENER: Token = Token(0);
+
 fn main() {
-    const SERVER: Token = Token(0);
+    let mut my_server = Server::new("127.0.0.1:8000").unwrap();
+    my_server.run();
+}
 
-    let addr = "127.0.0.1:13265".parse().unwrap();
+struct Server {
+    listener: TcpListener,
+    conns: Slab<TcpStream>
+}
 
-    // Setup the server socket
-    let server = TcpListener::bind(&addr).unwrap();
+impl Server {
+    fn new(addr: &str) -> io::Result<Self> {
+        let addr = addr.parse().expect("Unable to parse addr string.");
 
-    // Create a poll instance
-    let poll = Poll::new().unwrap();
+        let listener = TcpListener::bind(&addr)?;
+        let mut conns = Slab::with_capacity(128);
 
-    // Start listening for incoming connections
-    poll.register(&server, SERVER, Ready::readable(), PollOpt::edge()).unwrap();
+        conns.vacant_entry();
 
-    // Create storage for events
-    let mut events = Events::with_capacity(1024);
+        Ok(Self {
+            listener,
+            conns,
+        })
+    }
 
-    // Create storage for incoming clients
-    let mut streams = Vec::new();
+    fn run(&mut self) {
+        let poll = Poll::new().unwrap();
+        let mut events = Events::with_capacity(1024);
 
-    loop {
-        poll.poll(&mut events, None).unwrap();
+        poll.register(&self.listener, LISTENER, Ready::readable(), PollOpt::edge()).unwrap();
 
-        for event in events.iter() {
-            match event.token() {
-                SERVER => {
-                    let (stream, _) = server.accept().unwrap();
+        loop {
+            poll.poll(&mut events, None).unwrap();
 
-                    // Should be given a better token
-                    let client_token = Token(streams.len() + 1);
+            for event in &events {
+                println!("{:?}", event);
+                match event.token() {
+                    LISTENER => {
+                        loop {
+                            match self.listener.accept() {
+                                Ok((socket, _)) => {
+                                    let entry = self.conns.vacant_entry();
+                                    // To allow server to have Token(0)
+                                    let token = Token(entry.key() + 1);
 
-                    poll.register(&stream, client_token, Ready::readable(), PollOpt::edge()).unwrap();
-
-                    streams.push(stream);
-                }
-                Token(n) => {
-                    #[cfg(all(unix, not(target_os = "fuchsia")))]
-                    {
-                        use mio::unix::{UnixReady};
-
-                        // Client dropped
-                        // https://carllerche.github.io/mio/mio/unix/struct.UnixReady.html#method.hup
-                        if UnixReady::from(event.readiness()).is_hup() {
-                            println!("Client {} dropped", n);
+                                    poll.register(&socket, token, Ready::readable(), PollOpt::edge()).unwrap();
+                                    entry.insert(socket);
+                                }
+                                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => break,
+                                Err(ref e) => panic!("{}", e)
+                            }
                         }
                     }
-                    
-                    let mut stream = &streams[n - 1];
 
-                    // Very tiny, just to make sure the loop below works
-                    let mut buf = [0; 8];
-                    let mut out = Vec::new();
+                    Token(n) => {
+                        // To allow server to have Token(0)
+                        let n = n - 1;
 
-                    loop {
-                        match stream.read(&mut buf) {
-                            Ok(n) if n != 0 => out.extend_from_slice(&buf[..n]),
-                            _ => break,
+                        let mut buf = [0; 8];
+                        let mut out = Vec::new();
+
+                        loop {
+                            match self.conns.get(n).unwrap().read(&mut buf) {
+                                Ok(0) => {
+                                    self.conns.remove(n);
+                                    break;
+                                }
+                                Ok(n) => {
+                                    out.extend_from_slice(&buf[..n]);
+                                }
+                                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => break,
+                                Err(ref e) => panic!("{}", e),
+                            }
                         }
-                    }
-                    
-                    if out.len() == 0 {
-                        println!("Client {} dropped", n);
-                    }
 
-                    let out = std::str::from_utf8(&out).unwrap().trim();
-                    if out.len() > 0 {
-                        println!("Client {}: {}", n, out);
+                        let out = std::str::from_utf8(&out).unwrap().trim();
+                        println!("{}", out);
                     }
                 }
             }
